0d2f41bde52ac0240fc4ec6f40bce6b51059b2b5
Nick Raptis <nraptis@gmail.com>
2019-10-26 15:09:12 -0700

    Very simple class, asset wad. This essentially represents a group of related assets. The best use-case for this as of now is variable resolution graphics.

---
+++ Framework/[C++ Core]/FAssetWad.cpp
@@ -0,0 +1,25 @@
+//
+//  AssetWad.cpp
+//  Crazy Darts 2 Mac
+//
+//  Created by Nicholas Raptis on 8/24/19.
+//  Copyright © 2019 Froggy Studios. All rights reserved.
+//
+
+#include "FAssetWad.hpp"
+
+FAssetWad::FAssetWad() {
+    
+}
+
+FAssetWad::~FAssetWad() {
+    Unload();
+}
+
+void FAssetWad::Load() {
+    
+}
+
+void FAssetWad::Unload() {
+    
+}
