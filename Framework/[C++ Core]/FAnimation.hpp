fac9ee8a48db06f676ddcf4e5e26ab95af3b2b40
Nick Raptis <nraptis@gmail.com>
2020-06-11 22:57:03 -0700

    Cleaning and formatting Animation class.

--- Framework/[C++ Core]/FAnimation.hpp
+++ Framework/[C++ Core]/FAnimation.hpp
@@ -66,83 +66,13 @@
 //
 /////////////////////////////
 
-
-
-
-
-
-
-
 #include "FVec2.hpp"
 
-class FAnimation
-{
+class FAnimation {
 public:
     
     FAnimation();
     virtual ~FAnimation();
-    
-    
-    /*
-    void                                            Clear();
-    void                                            Reset();
-
-    //void                                            AddValue(float pValue, int pDimension);
-    
-    
-    void                                            AddValue(float pX);
-    void                                            AddValue(float pX, float pY);
-    void                                            AddValue(float pX, float pY, float pZ);
-    
-    int                                             GetCount();
-    
-    float                                           GetX(int pTick);
-    float                                           GetY(int pTick);
-    float                                           GetZ(int pTick);
-    float                                           Get(int pTick, int pDim);
-    
-    void                                            Draw(int pTick=-1);
-    
-    void                                            Resample(int pTicks);
-    void                                            Clone(FAnimation *pAnimation);
-    void                                            EnsureSize(int pSize, int pDimensions);
-    
-    
-    float                                           GetLength2D();
-    
-    void                                            CreateLinear2D(float pStartX, float pStartY, float pEndX, float pEndY, int pTicks=100);
-    void                                            CreateLinear(float pStart, float pEnd, int pTicks, int pDim);
-    
-    //Alright, "Speed" is for sure not the right word here, it's the tangent... which, isn't the same..
-    void                                            CreateSpline2D(FVec2 pStart, FVec2 pEnd, FVec2 pStartTan, FVec2 pEndTan, int pTicks=100);
-    void                                            CreateSpline(float pStart, float pEnd, float pStartTan, float pEndTan, int pDim, int pTicks=100);
-    
-    void                                            CreateDeceleration(FVec2 pStart, FVec2 pEnd, FVec2 pStartSpeed, FVec2 pEndSpeed);
-    void                                            CreateStartStopSpeed(FVec2 pStart, FVec2 pEnd, FVec2 pStartSpeed, FVec2 pEndSpeed);
-    void                                            CreateBouncingHalt(FVec2 pStart, FVec2 pEnd, FVec2 pStartSpeed);
-    */
-    
-    /*
-    Solve(mX,aStore,mTangent,aStore+mPointCount,mXCoef,linear,circular);
-
-        if(!aDer[0])theDerivative[0]=0;
-        if(!aDer[1])theDerivative[1]=0;
-        theCoef[aCoefOffStart++]=theDerivative[0];
-        theCoef[aCoefOffStart++]=3*(theCoordinate[1]-theCoordinate[0])-2*theDerivative[0]-theDerivative[1];
-        theCoef[aCoefOffStart++]=2*(theCoordinate[0]-theCoordinate[1])+theDerivative[0]+theDerivative[1];
-    */
-    
-    /*
-    float                                           **mValue;
-    
-    int                                             mValueCount;
-    int                                             mValueSize;
-    
-    //x, y, z...  (AFAIK, we are only gonna be using 2d x & y ones, but hey, who knowz dawsszgaw -
-    int                                             mDimensions;
-    
-    */
-    
     
     static float                                    SplineInterpolate(float pPercent, float pStartValue, float pEndValue, float pStartTan, float pEndTan);
     
@@ -150,9 +80,6 @@ public:
     
     
 	static float									ApplyFunction(int pFunction, float pPercent);
-	static FString									GetFunctionNameExtended(int pFunction);
-
-
     
     //The sexiest ones:
     static float                                   EaseInOutExpo(float pPercent);
@@ -171,7 +98,6 @@ public:
     static float                                   EaseInOutQuint(float pPercent);
     static float                                   EaseOutInQuint(float pPercent);
     
-    
     static float                                   EaseInSine(float pPercent);
     static float                                   EaseOutSine(float pPercent);
     static float                                   EaseInOutSine(float pPercent);
@@ -185,25 +111,20 @@ public:
     static float                                   EaseInCurve(float pPercent);
     static float                                   EaseSineCurve(float pPercent);
     
-    static float                                   EaseOutElastic(float pPercent, float pAmplitude=1.0f, float pPeriod=0.5f);
-    
+    static float                                   EaseOutElastic(float pPercent, float pAmplitude = 1.0f, float pPeriod = 0.5f);
     
     static float                                   EaseOutBack(float pPercent, float pOvershoot);
-    static float                                   Overshoot(float pPercent){return EaseOutBack(pPercent, 1.70158f);}
-    static float                                   OvershootMore(float pPercent){return EaseOutBack(pPercent, 2.5f);}
+    static float                                   Overshoot(float pPercent) { return EaseOutBack(pPercent, 1.70158f); }
+    static float                                   OvershootMore(float pPercent) { return EaseOutBack(pPercent, 2.5f); }
     
     static float                                   EaseOutBounce(float pPercent, float pAmplitude);
-    static float                                   Bounce(float pPercent){return EaseOutBounce(pPercent, 0.23f);}
-    static float                                   BounceMore(float pPercent){return EaseOutBounce(pPercent, 0.5f);}
+    static float                                   Bounce(float pPercent) { return EaseOutBounce(pPercent, 0.23f); }
+    static float                                   BounceMore(float pPercent) { return EaseOutBounce(pPercent, 0.5f); }
     
     static float                                   HelperEaseOutElastic(float pPercent, float b, float c, float d, float pAmplitude, float pPeriod);
     static float                                   HelperEaseOutBounce(float pPercent, float c, float pAmplitude);
     static float                                   HelperEaseSinProgress(float pValue);
     static float                                   HelperEaseSmoothBeginEndMixFactor(float pValue);
-
-
-
-
 };
 
 #endif
